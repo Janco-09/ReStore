e27dee069e1e25022737013e590f037f0617fb93
Janco <enversoft.de_beer@mercedes-benz.com>
2022-11-22 16:24:51 +0200

    Added components with dummy data

---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,37 @@
+import Catalog from "../../features/catalog/catalog";
+import Header from "./Header";
+import { createTheme, CssBaseline } from "@mui/material";
+import { Container } from "@mui/system";
+import { ThemeProvider } from "@emotion/react";
+import { useState } from "react";
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const paletteType = darkMode ? "dark" : "light";
+
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  const handleThemeChange = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
